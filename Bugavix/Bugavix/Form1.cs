e1c8938501ce2815693eaba512f05a3d42e87f05
alexhause <52361639+alexhause@users.noreply.github.com>
2021-04-19 01:05:59 +0300

    bottonClick
    Добавил вывод сообщения по кнопке

--- Bugavix/Bugavix/Form1.cs
+++ Bugavix/Bugavix/Form1.cs
@@ -17,5 +17,9 @@ namespace Bugavix
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("WOW!");
+        }
     }
 }
